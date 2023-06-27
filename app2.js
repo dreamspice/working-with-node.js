85329dcc267fd55ab70839d130aa66e6abed3371
dreamspice <basicuser00@protonmail.com>
2023-06-27 17:18:28 +0200

    uderstanding basic of express.js

--- app2.js
+++ app2.js
@@ -1,3 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = 
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Send</button></form>"
+  );
+});
+
+app.use('/product', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res) => {
+  res.send('<h1>Hello from express</h1>');
+});
+app.listen(3000);
