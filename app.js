52be655f256970fc5b3238e0fbb1dfcf03ee63f3
dreamspice <basicuser00@protonmail.com>
2023-06-28 16:06:46 +0200

    Learning about Templating Engines.

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const path = require('path');
+
 const bodyParser = require('body-parser');
+const rootDir = require('./helpers/path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,11 @@ const rootDir = require('../helpers/path');
 
 const router = express.Router();
 
+const adminData = require('./admin');
+const products = adminData.products;
+
 router.get('/', (req, res, next) => {
+  console.log('shop.js', products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
